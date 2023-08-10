e2b457749530d963a1b56db8f130f8e2e802d632
Arthur Burkhard Ritzel <arthurritzel@MacBook-Air-de-Arthur.local>
2023-08-09 21:09:35 -0300

    lista alocacao dinamica

---
+++ lista 3 alocacao dinamica/exe005.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *vet, aux, i;
+
+    printf("Digite valores a serem armazendos em um vetor, digite um numero negativo para sair");
+    for (i = 0; i != -1; i++){
+        printf("->");
+        scanf("%i", &aux);
+        if (aux < 0){
+            break;
+        }else{
+            if (i == 0){
+                vet = (int *)malloc(sizeof(float));
+                if(vet == NULL){
+                    printf("Erro na criacao do arquivp");
+                    return 0;
+                }
+                vet[0] = aux;
+            }else{
+                vet = (int *)realloc(vet, i * sizeof(float));
+                vet[i] = aux;
+            }
+        }
+    }
+
+    if(vet == NULL){
+        printf("Nenhum valor armazenado!");
+    }else{
+        for(int j = 0; j < i; j++){
+            printf("%i", vet[j]);
+        }
+    }
+    
+}
